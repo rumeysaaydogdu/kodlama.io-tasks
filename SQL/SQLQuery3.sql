c811250fe95d77c55091a0563e7165671f7fb158
Rumeysa <aydogdurumeysa99@hotmail.com>
2021-01-31 17:55:52 +0300

    sql query

---
+++ SQL/SQLQuery3.sql
@@ -0,0 +1,3 @@
+﻿Select ProductName [Ürün Adı], Sum(od.unitPrice * od.Quantity)[Kazanılan Toplam Miktar]
+from Products p inner join [Order Details]  od on p.ProductID = od.ProductID 
+inner join Orders o on o.OrderID = od.OrderID group by ProductName
