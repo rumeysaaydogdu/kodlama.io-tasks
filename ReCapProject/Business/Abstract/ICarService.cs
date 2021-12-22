ec066054fd1b868064ed1c7573a6a0defb6f5c2a
Rumeysa <aydogdurumeysa99@hotmail.com>
2021-12-22 11:38:23 +0300

    day 7 - 22 December 2021

---
+++ ReCapProject/Business/Abstract/ICarService.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Concrete;  // Önce referans olarak Entites ve DataAccess katmanını projeye ekledik.
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ICarService
+    {
+        List<Car> GetAll();
+    }
+}
---
+++ ReCapProject/DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;  // Önce referans olarak Entites katmanını projeye ekledik.
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface ICarDal
+    {
+        List<Car> GetAll();
+        void Add(Car car);
+        void Delete(Car car);
+        void Update(Car car);
+        List<Car> GetAllByBrand(int brandId);
+    }
+}
